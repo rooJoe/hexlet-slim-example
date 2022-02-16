d0e422993077193f5ee7ebbe05505abb42f2d8f3
e.fomkin <97235032+rooJoe@users.noreply.github.com>
2022-02-16 04:50:34 +0200

    2

--- public/index.php
+++ public/index.php
@@ -3,14 +3,16 @@
 // Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
-use Slim\Factory\AppFactory;
 
+use Slim\Factory\AppFactory;
+echo __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+  //  return $response->write('Welcome to Slim!');
+    //$response->getBody()->write('Welcome to Slim!');
+    //return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
