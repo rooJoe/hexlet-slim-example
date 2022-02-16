c386dac9d55a7b1aa241ca9dd45957e8b98925e5
e.fomkin <97235032+rooJoe@users.noreply.github.com>
2022-02-16 05:00:20 +0200

    3

--- public/index.php
+++ public/index.php
@@ -5,12 +5,12 @@ require __DIR__ . '/../vendor/autoload.php';
 
 
 use Slim\Factory\AppFactory;
-echo __DIR__ . '/../vendor/autoload.php';
+echo __DIR__;
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-  //  return $response->write('Welcome to Slim!');
+    return $response->write('Welcome to Slim!');
     //$response->getBody()->write('Welcome to Slim!');
     //return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
